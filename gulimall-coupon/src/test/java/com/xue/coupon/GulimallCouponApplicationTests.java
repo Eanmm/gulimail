5fc59b50cc7feefa0fd917c1f3c9170f78655f5c
Eanmm <2662343801@qq.com>
2024-05-09 14:19:40 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/xue/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xue.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
